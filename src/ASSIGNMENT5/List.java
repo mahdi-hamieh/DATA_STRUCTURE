31e038492cf13b4ca4334abd10c76f83a27078d6
mahdi hamieh <mah113@live.aul.edu.lb>
2020-12-21 13:18:07 +0200

    assignment5

---
+++ src/ASSIGNMENT5/List.java
@@ -0,0 +1,16 @@
+package ASSIGNMENT5;
+public interface List {
+
+    void add(Object newElement);
+
+    Object get(int index);
+
+    int indexOf(Object element);
+
+    Object remove(int index);
+
+    boolean remove(Object element);
+
+    int size();
+
+}
